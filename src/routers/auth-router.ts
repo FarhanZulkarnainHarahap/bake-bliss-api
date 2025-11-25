3ffcb559e734ccfe8b85f065aff2fcb14713873d
NexxusFz <farhanzulkarnaenhrp@gmail.com>
2025-11-25 21:03:29 +0700

    fix import router login

--- src/routers/auth-router.ts
+++ src/routers/auth-router.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import { register } from "../controllers/user-controller/register/register-controller.js";
-import { login } from "@/controllers/user-controller/login/login-controller.js";
+import { login } from "../controllers/user-controller/login/login-controller.js";
 
 const router = express.Router();
 
