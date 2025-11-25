f5c2c200283caa0101df04527d6707fe4d55021b
NexxusFz <farhanzulkarnaenhrp@gmail.com>
2025-11-25 15:27:00 +0700

    fix app1

--- src/app.ts
+++ src/app.ts
@@ -6,10 +6,10 @@ import authRouter from "./routers/auth-router.js";
 
 const app: Application = express();
 
-const PORT: string = process.env.PORT || "8000";
+const PORT: number = (process.env.PORT as unknown as number) || 8000;
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://bake-bliss.app"], // Menambahkan localhost untuk development
+    // origin: ["http://localhost:3000", "https://bake-bliss.app"], // Menambahkan localhost untuk development
     methods: ["GET", "POST", "PUT", "DELETE"], // Atur metode HTTP yang diizinkan
     credentials: true, // Mengizinkan pengiriman cookies dan headers
   })
@@ -25,8 +25,8 @@ app.get("/", async (_request: Request, response: Response) => {
     response.status(500).json({ message: "Terjadi kesalahan pada server" });
   }
 });
-// app.listen(PORT, () =>
-//   console.info(` 🚀 Server is listening on port: ${PORT}`)
-// );
+app.listen(PORT, () =>
+  console.info(` 🚀 Server is listening on port: ${PORT}`)
+);
 
 export default app;
