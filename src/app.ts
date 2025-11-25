b27515b91f813eb67434f0897dc882d096fe7c1a
NexxusFz <farhanzulkarnaenhrp@gmail.com>
2025-11-25 17:19:34 +0700

    fixx app

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
-import express, { Request, Response, Application } from "express";
 import "dotenv/config";
+import express, { Request, Response, Application } from "express";
 import cors from "cors";
 
 import authRouter from "./routers/auth-router.js";
@@ -9,7 +9,7 @@ const app: Application = express();
 const PORT: number = (process.env.PORT as unknown as number) || 8000;
 app.use(
   cors({
-    // origin: ["http://localhost:3000", "https://bake-bliss.app"], // Menambahkan localhost untuk development
+    origin: ["http://localhost:3000", "https://bake-bliss.app"], // Menambahkan localhost untuk development
     methods: ["GET", "POST", "PUT", "DELETE"], // Atur metode HTTP yang diizinkan
     credentials: true, // Mengizinkan pengiriman cookies dan headers
   })
@@ -17,16 +17,16 @@ app.use(
 app.use(express.json());
 
 app.use("/api/auth", authRouter);
-app.get("/", async (_request: Request, response: Response) => {
+app.get("/", async (_req: Request, res: Response) => {
   try {
-    response.send("API is running...");
+    res.send("API is running...");
   } catch (error) {
     console.error(error);
-    response.status(500).json({ message: "Terjadi kesalahan pada server" });
+    res.status(500).json({ message: "Terjadi kesalahan pada server" });
   }
 });
 app.listen(PORT, () =>
-  console.info(` 🚀 Server is listening on port: ${PORT}`)
+  console.info(` 🚀 Server is listening on port: http://localhost:${PORT}`)
 );
 
 export default app;
