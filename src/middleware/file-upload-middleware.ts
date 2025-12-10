e3f00e53e898d7f5e3d5f8378016d177ca569bf9
NexxusFz <farhanzulkarnaenhrp@gmail.com>
2025-12-10 14:57:10 +0700

    fix fileupload cloudinary

--- src/middleware/file-upload-middleware.ts
+++ src/middleware/file-upload-middleware.ts
@@ -2,16 +2,6 @@ import multer from "multer";
 import path from "path";
 
 export const fileUpload = multer({
-  storage: multer.diskStorage({
-    destination: (req, file, cb) => {
-      cb(null, "public/image");
-    },
-    filename: (req, file, cb) => {
-      const fileName = `${Date.now()}-${Math.random()
-        .toString(36)
-        .slice(2, 6)}`;
-      cb(null, fileName + path.extname(file.originalname));
-    },
-  }),
+  storage: multer.memoryStorage(),
   limits: { fileSize: 25 * 1024 * 1024 },
 });
