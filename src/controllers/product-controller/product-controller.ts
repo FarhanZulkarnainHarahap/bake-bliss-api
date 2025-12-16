2e52820781173a68d66e004f13f289f721a51039
NexxusFz <farhanzulkarnaenhrp@gmail.com>
2025-12-16 10:56:00 +0700

    fix create  product

--- src/controllers/product-controller/product-controller.ts
+++ src/controllers/product-controller/product-controller.ts
@@ -151,29 +151,10 @@ export async function getAllProducts(req: Request, res: Response) {
   try {
     const products = await prisma.product.findMany({
       include: {
-        productImages: true,
+        productImages: { include: { ImagePreview: true, ImageContent: true } },
       },
     });
 
-    // const resultProducts = products.map((product) => {
-    //   const imagePreview: { id: string | null; url: string }[] = [];
-    //   const imageContent: { id: string | null; url: string }[] = [];
-    //   for (const el of product.productImages) {
-    //     if (el.ImageContent?.id && el.ImageContent?.url) {
-    //       imageContent.push({
-    //         id: el.ImageContent.id,
-    //         url: el.ImageContent.url,
-    //       });
-    //     }
-
-    //     if (el.ImagePreview?.id && el.ImagePreview?.url) {
-    //       imagePreview.push({
-    //         id: el.ImagePreview.id || null,
-    //         url: el.ImagePreview.url,
-    //       });
-    //     }
-    //   }
-    // });
     res.status(200).json({
       message: "Success get all products",
       data: products,
