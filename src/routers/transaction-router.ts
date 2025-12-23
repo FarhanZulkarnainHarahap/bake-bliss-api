19d96f3fe64f9777552916cbb7882676e6129bbe
NexxusFz <farhanzulkarnaenhrp@gmail.com>
2025-12-23 08:42:58 +0700

    fix transaction router

--- src/routers/transaction-router.ts
+++ src/routers/transaction-router.ts
@@ -3,7 +3,7 @@ import {
   createTransaction,
   getOrderByCode,
 } from "../controllers/transaction-controller/transaction-controller.js";
-import { verifyToken } from "@/middleware/auth-middleware.js";
+import { verifyToken } from "../middleware/auth-middleware.js";
 
 const router = express.Router();
 
